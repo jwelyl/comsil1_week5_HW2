71cefd564d44749ee76c9e3a2b9ffb9ad5478811
jwelyl <cork2586@naver.com>
2020-10-07 02:38:51 +0900

    VS2019 GitHub

---
+++ GitHubPrj/GitHubPrj/VSGitHubcpp.cpp
@@ -0,0 +1,6 @@
+#include <cstdio>
+
+int main(void) {
+	printf("VS2019 and GitHub \n");
+	return 0;
+}
---
+++ source.cpp
@@ -0,0 +1,6 @@
+#include <cstdio>
+
+int main(void) {
+	printf("GitHub, VS2019\n");
+	return 0;
+}
